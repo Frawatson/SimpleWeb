ed94df52fedfad92656b5c8f3682c4119c070968
Frawatson <franciswatson9@gmail.com>
2023-06-22 17:05:03 -0500

    Simple web project

---
+++ src/test/java/com/Sanders/Fresco/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sanders.Fresco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
